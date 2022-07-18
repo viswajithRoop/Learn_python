37df00a33957540f7ad42dd06ff6b8c932a7db85
viswajith <viswajithroop1@gmail.com>
2022-07-18 22:58:36 +0530

    completed upto ex3

---
+++ print.py
@@ -0,0 +1,17 @@
+print("hello world!!")
+print("my name is viswajith")
+print("i like typing this")
+print("This is fun")
+print("Yay! printing.")
+print('i "said" do not touch thisl')
+
+
+print("I could have code like this") #and the comment after is ignored
+#print("this won't run")
+print("this will run")
+
+
+print("Hens" , 25 + 30 / 6)
+print(3+2 < 5-7)
+print("is it greater ?", 5>-2)
+print("is it equal or less?", 5 <= -2)
