f14a7eebeb26d35527cc80c96969c15f6d32e5ee
viswajith <viswajithroop1@gmail.com>
2022-07-20 11:50:04 +0530

    ex 11,12&13 completed

---
+++ ex11,12,13.py
@@ -0,0 +1,28 @@
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"so, you're {age} old , {height} tall and {weight} heavy")
+
+print()
+
+age = input("How old are you? ")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print(f"So, you're {age} old, {height} tall and and {weight} heavy")
+
+print()
+
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is :", first)
+print("Your second variable is:", second)
+print("Your third variable is :", third)
+
