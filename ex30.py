6ab4f9450b3bd109c88e169e5f075c87f87c2cb3
viswajith <viswajithroop1@gmail.com>
2022-07-22 16:17:20 +0530

    ex 25,26,27,29&30

---
+++ ex30.py
@@ -0,0 +1,24 @@
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print("we should take the cars.")
+elif cars < people:
+    print("we should not take the cars.")
+else:
+    print("we can't decide")
+
+if trucks > cars:
+    print("That's too many trucks.")
+elif trucks < cars:
+    print("Maybe we could take the trucks.")
+else:
+    print("we still can't decide.")
+
+if people > trucks:
+    print("Alright, let's just take the trucks.")
+else:
+    print("Fine, let's stay home then.")
+
+
