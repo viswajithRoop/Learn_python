1bba4dd955dcf9af4b4f447e4e0e3d7de2ced106
viswajith <viswajithroop1@gmail.com>
2022-07-27 10:47:44 +0530

    ex35,38,39&40 completd

--- ex35.py
+++ ex35.py
@@ -7,13 +7,14 @@ def gold_room():
     choice = input(">")
     if "0" in choice or "1" in choice:
         how_much = int(choice)
-        if how_much < 50:
-            print("Nice, you're not greedy, you win!")
-            exit(0)
-        else:
-            dead("You greedy maahn!")
+
     else:
         dead("Man, learn to type a number.")
+    if how_much < 50:
+        print("Nice, you're not greedy, you win!")
+        exit(0)
+    else:
+        dead("You greedy maahn!")
     
     
 
